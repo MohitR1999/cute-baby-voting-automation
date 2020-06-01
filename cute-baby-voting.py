202ef70d73434bba3a7b3b17f6f2c7d6116707b1
Mohit Ranjan <mohitr1999@gmail.com>
2020-06-01 17:37:34 +0530

    Some improvements to the command line UI :)

--- cute-baby-voting.py
+++ cute-baby-voting.py
@@ -1,9 +1,18 @@
 import time
 from selenium.webdriver import Chrome
 #Write the URL here
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        print(timeformat, end='\r')
+        time.sleep(1)
+        t -= 1
+
 URL = 'https://mycutebaby.in/contest/participant/?n=5ebe684e8e100'
 #Write your name here
 NAME = 'Mohit Ranjan'
+count = 0
 while(1):
     #Open URL
     browser = Chrome(executable_path='./chromedriver')
@@ -11,12 +20,18 @@ while(1):
     #Fill Info
     nameInput = browser.find_element_by_id('v')
     voteButton = browser.find_element_by_id('vote_btn')
-    time.sleep(10)
+    #Allow page to load completely. This delay should be adjusted as per your internet speed.
+    print('Page load hone dete hai')
+    countdown(10)
     #Press button to vote
     nameInput.send_keys(NAME)
-    voteButton.click()
+    # voteButton.click()
+    count = count + 1
+    print(str(count) + ' vote done')
     #sleep for 30 minutes
-    time.sleep(30*60)
+    print('Sleeping for 30 minutes now...')
+    countdown(30*60)
     #close the browser and restart
+    print('Chaliye shuru krte hai :)')
     browser.close()
 
